a52030075f94a52cece0b60afb7b76cebf780017
da1002019 <da1002019@RK03-B1225-09.francistuttle.edu>
2018-01-19 08:29:47 -0600

    p7FirstnameLastname

---
+++ src/a7DanielAnderson.java
@@ -0,0 +1,47 @@
+//Daniel Anderson
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class a7DanielAnderson {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many numbers will be read: ");
+        int a[] = new int[input.nextInt()];
+        numbers(a);
+
+        System.out.println("The average is: " + average(a));
+        System.out.println("The contents of the array: ");
+        System.out.println(java.util.Arrays.toString(a));
+    }
+
+    public static void numbers(int a[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter elements: ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = input.nextInt();
+        }
+        average(a);
+    }
+
+    public static void print(int[] a) {
+
+    }
+
+    public static int average(int a[]) {
+        int total = 0;
+        for (int ar : a) {
+            total += ar;
+
+        }
+        return total / a.length;
+    }
+
+    public static void printing(int a[]) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < a.length; i++) {
+
+        }
+    }
+}
